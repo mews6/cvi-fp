5a2dee190f5d4609ab5cd9efe3cc7f703020f412
Jaime Torres <156240086+mews6@users.noreply.github.com>
2025-05-27 15:19:58 -0500

    add second model

--- obj-converter/convert.js
+++ obj-converter/convert.js
@@ -5,4 +5,8 @@ const options = {
 };
 obj2gltf("005_Foliage_OBJ/005_Foliage_OBJ.obj", options).then(function (glb) {
   fs.writeFileSync("../assets/models/005_Foliage_OBJ/model.glb", glb);
-});
+}); 
+
+obj2gltf("dry-leaf-rawscan/source/foglia/foglia/foglia.obj", options).then(function (glb) {
+  fs.writeFileSync("../assets/models/foglia/model.glb", glb);
+}); 
