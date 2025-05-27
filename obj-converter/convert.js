b36d059b752c48479c7ae9ea2be0dde99cc8dbf1
Jaime Torres <156240086+mews6@users.noreply.github.com>
2025-05-27 14:22:28 -0500

    add some more things

--- obj-converter/convert.js
+++ obj-converter/convert.js
@@ -1,6 +1,6 @@
 const obj2gltf = require("obj2gltf");
 const fs = require("fs");
-obj2gltf("model.obj").then(function (gltf) {
+obj2gltf("005_Foliage_OBJ/005_Foliage_OBJ.obj").then(function (gltf) {
   const data = Buffer.from(JSON.stringify(gltf));
   fs.writeFileSync("model.gltf", data);
 });
